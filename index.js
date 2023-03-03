bb8dbe36b0c910f28ef3efda9728493071ffb25f
HSM009 <hoseinsirat@gmail.com>
2023-03-03 16:41:09 +0500

    Good Start

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-console.log("Sup");
-export {};
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalkAnimation from "chalk-animation";
+const stopTime = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3500);
+    });
+};
+async function welcome() {
+    let rainbowTitle = chalkAnimation.neon("Let's play Number Guessing Game!\n\nCoded By Hosein Sirat Mohammad");
+    await stopTime();
+    rainbowTitle.stop();
+}
+await welcome();
+async function ask() {
+    await inquirer
+        .prompt([
+        /* Pass your questions in here */
+        {
+            type: "input",
+            name: "name",
+            message: "what is your name?"
+        }
+    ])
+        .then((answers) => {
+        let a = answers.name;
+        console.log("Welcome Sir/Madam " + a.toLocaleUpperCase());
+    });
+}
+ask();
